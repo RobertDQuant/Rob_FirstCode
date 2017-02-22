ef550d7a857cf14bf0cfc6f43a2e4d4dd98123ba
RobertDQuant <Robertdquant@gmail.com>
2017-02-22 16:16:01 -0500

    RobFirstCode

---
+++ Rob_First Code/Ellipse Code/sketch.js
@@ -0,0 +1,15 @@
+//this is the code for how big the canvas is
+function setup() {
+  createCanvas(640, 480)
+ 
+}
+
+function draw() {
+//this is the code for the ellipse shape
+  ellipse(50,50,80,80);
+//this is the code for the rectangle shape
+  rect(30, 20, 80, 80);
+//this is the code for the triangle shape
+  triangle(30, 75, 58, 20, 86, 75);
+  
+}
---
+++ Rob_First Code/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() {
+  noStroke();
+  fill(0);
+  ellipse(300, 50, 50, 50);
+
+}
